9661c72c1c4a3aaff1bc5107996631bd4f3fa147
ska <1060450556@qq.com>
2026-02-26 13:09:14 +0800

    Initial Commit

---
+++ PomodoroWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PomodoroWidget
+//
+//  Created by 崔紫微 on 2026/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PomodoroWidget/PomodoroWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroWidgetApp.swift
+//  PomodoroWidget
+//
+//  Created by 崔紫微 on 2026/2/26.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
